d0ad130cad26f3544f466846c18f53e473c81d6d
Luka Radovanovic <lukadlm97@gmail.com>
2020-03-18 19:54:21 +0100

    Using prevState in Hooks

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import HookCounter from './component/HookCounter';
+import HookCounterTwo from './component/HookCounterTwo';
+// import HookCounter from './component/HookCounter';
 // import ClassCounter from './component/ClassCounter';
 
 function App() {
   return (
     <div className="App">
-      <HookCounter />
+      <HookCounterTwo />
+      {/* <HookCounter /> */}
      {/* <ClassCounter /> */}
     </div>
   );
